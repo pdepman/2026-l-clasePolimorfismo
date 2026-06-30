d603070ec7f8f0c9f7c98f05158b0c8da04330ae
asanzo <alfredo.sanzo@gmail.com>
2026-06-29 22:11:38 -0300

    Add player goal and team information in prode.pl

--- prode.pl
+++ prode.pl
@@ -1,1 +1,23 @@
 % Aquí va el código.
+
+% hizoGolContra(Jugador, EquipoContrario).
+hizoGolContra(messi, argelia).
+hizoGolContra(messi, argelia).
+hizoGolContra(messi, argelia).
+hizoGolContra(haaland, senegal).
+hizoGolContra(haaland, senegal).
+hizoGolContra(pedersen, senegal).
+hizoGolContra(sar, noruega).
+hizoGolContra(sar, noruega).
+
+% equipoDe(Jugador, Equipo).
+equipoDe(messi, argentina).
+equipoDe(haaland, noruega).
+equipoDe(pedersen, noruega).
+equipoDe(sar, senegal).
+
+% Vemos el futuro:
+campeonMundial(marruecos).
+
+
+
